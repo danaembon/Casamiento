1e16e3a6a3e2d59ecc015e1c09af1ac0ddf7b153
danaembon <danaembon10@gmail.com>
2024-06-25 00:14:04 +0300

    Desplegar app en Heroku

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+pp.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hola desde el servidor!" });
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
