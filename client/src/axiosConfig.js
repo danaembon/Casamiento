457009754e53c5a40fbcff785ce146733f61bb90
danaembon <danaembon10@gmail.com>
2024-07-06 19:29:35 +0300

    Fixed corsOptions not defined error

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,10 +1,7 @@
-// axios.js
-
 import axios from 'axios';
-// Asegúrate de ajustar la ruta según tu estructura de archivos
 
 const instance = axios.create({
-  baseURL: `http://localhost:${process.env.PORT || 3001}`,
+  baseURL: process.env.REACT_APP_BASE_URL || `http://localhost:${process.env.PORT || 3001}`,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
