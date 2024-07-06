ec28409acba56786c98f0caa1b9b49a595721c7a
danaembon <danaembon10@gmail.com>
2024-07-06 16:19:42 +0300

    actualizar port

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-const PORT = process.env.PORT || 3001;
 // Creamos una instancia de Axios con la configuración base
 const instance = axios.create({
-  baseURL: `http://localhost:${PORT}`,
+  baseURL: `http://localhost:${process.env.PORT || 3001}`,
   timeout: 5000, // Timeout de 5 segundos
   headers: {
     'Content-Type': 'application/json', // Tipo de contenido JSON por defecto
