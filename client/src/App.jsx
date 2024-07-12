c974d3528c51a48bdfe4ff68b4156eb53997f8e0
danaembon <danaembon10@gmail.com>
2024-07-12 08:05:19 +0300

    agregando favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -80,7 +80,7 @@ function App() {
          <div>
           <p>¿Como llegar?</p>
           <div>
-          <iframe title='ubicacion al casamiento' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3290.195468048256!2d-58.79825969999999!3d-34.4471857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9ecf9c7c2583%3A0x81b2e7a09d3c0dda!2sFinca%20Madero%20El%20Omb%C3%BA!5e0!3m2!1ses-419!2sil!4v1718443574799!5m2!1ses-419!2sil" width="600" height="400" style={{border:0,  allowfullscreen: "", loading: "lazy", referrerpolicy: "no-referrer-when-downgrade"}}></iframe>
+          <iframe title='ubicacion al casamiento' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3290.195468048256!2d-58.79825969999999!3d-34.4471857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9ecf9c7c2583%3A0x81b2e7a09d3c0dda!2sFinca%20Madero%20El%20Omb%C3%BA!5e0!3m2!1ses-419!2sil!4v1718443574799!5m2!1ses-419!2sil" style={{border:0,  allowfullscreen: "", loading: "lazy", referrerpolicy: "no-referrer-when-downgrade"}}></iframe>
           </div>
          </div>
         </article>
