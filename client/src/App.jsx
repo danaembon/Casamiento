29771c0ed003ee11dc1396299cdbc3a10499a2d8
danaembon <danaembon10@gmail.com>
2024-07-12 08:45:21 +0300

    estilando informacion y arreglando centrado de navegacion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -78,29 +78,27 @@ function App() {
           <div className="info-container">
             <div className="info-item">
               <h3>17 de Noviembre</h3>
-              <hr className="linea-separadora-informacion" />
             </div>
             <div className="info-item">
               <h3>Finca Madero - Pilar</h3>
-              <hr className="linea-separadora-informacion" />
             </div>
-            <div className="info-item">
+            <div className="info-item full-width">
               <p>¿Cómo llegar?</p>
-              <hr className="linea-separadora-informacion" />
             </div>
-          </div>
-          <div className="map-container">
-            <iframe 
-              title="ubicacion al casamiento" 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3290.195468048256!2d-58.79825969999999!3d-34.4471857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9ecf9c7c2583%3A0x81b2e7a09d3c0dda!2sFinca%20Madero%20El%20Omb%C3%BA!5e0!3m2!1ses-419!2sil!4v1718443574799!5m2!1ses-419!2sil" 
-              style={{ border: 0 }} 
-              allowFullScreen="" 
-              loading="lazy" 
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+            <div className="map-container">
+              <iframe 
+                title="ubicacion al casamiento" 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3290.195468048256!2d-58.79825969999999!3d-34.4471857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9ecf9c7c2583%3A0x81b2e7a09d3c0dda!2sFinca%20Madero%20El%20Omb%C3%BA!5e0!3m2!1ses-419!2sil!4v1718443574799!5m2!1ses-419!2sil" 
+                style={{ border: 0 }} 
+                allowFullScreen="" 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </article>
       </section>
+
       <section id='confirmar' className='sectionConfirmar'>
         <h2>Confirmar Asistencia</h2>
         <h3>Se puede confirmar hasta el 1 de Agosto</h3>
