4f38d5035217e594f1a56c366af81a26867172c4
danaembon <danaembon10@gmail.com>
2024-07-13 21:05:15 +0300

    centrando  iconos

--- client/src/logica/regalo.jsx
+++ client/src/logica/regalo.jsx
@@ -12,7 +12,7 @@ const Regalo = () => {
 
         <p className='p-regalo datos' onClick={handleIconClick}>Si deseas regalarnos algo más que tu hermosa presencia...</p>
         <span className='icono-regalo' onClick={handleIconClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-gift" width="60" height="60" viewBox="30 30 30 30" stroke-width="1.5" stroke="#f1eded" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-gift" width="60" height="60" viewBox="0s 0 30 30" stroke-width="1.5" stroke="#f1eded" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M3 8m0 1a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1z" />
         <path d="M12 8l0 13" />
