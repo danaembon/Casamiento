04114997e0b91bdc530ce30bf5e01dc40eb65865
danaembon <danaembon10@gmail.com>
2024-08-06 01:06:06 +0300

    agregando sobre

--- client/src/logica/regalo.jsx
+++ client/src/logica/regalo.jsx
@@ -24,7 +24,7 @@ const Regalo = () => {
         <button onClick={handleIconClick} className="ver-mas-button">Ver más</button>
         </div>
         {mostrarRegalo && ( 
-            <div>
+            <div className='contenedor-sobre'>
             <span className='icono-sobre'>
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#f1eded" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
